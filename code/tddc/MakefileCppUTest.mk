758b6ed7dc12f35c72a055b1bd4f15dae80f9b58
franklinted <stranger_2000@163.com>
2018-01-01 08:31:14 +0800

    franklin modify and first commit tddc

---
+++ code/tddc/MakefileCppUTest.mk
@@ -0,0 +1,42 @@
+#This makefile makes all the main book code with CppUTest test harness
+
+#Set this to @ to keep the makefile quiet
+SILENCE = @
+
+#---- Outputs ----#
+COMPONENT_NAME = BookCode_CppUTest
+
+#--- Inputs ----#
+CPP_PLATFORM = Gcc
+PROJECT_HOME_DIR = .
+SRC_DIRS = \
+	src/LedDriver \
+
+TEST_SRC_DIRS = \
+	tests\
+	tests/LedDriver\
+	
+INCLUDE_DIRS =\
+  .\
+  $(CPPUTEST_HOME)/include\
+  include/IO\
+  mocks\
+  include/util\
+  include/HomeAutomation\
+  include/LedDriver\
+  include/MyOS\
+  include/dvr\
+  include/zune\
+
+	
+CPPUTEST_WARNINGFLAGS = -Wall -Wswitch-default -Werror 
+#CPPUTEST_CFLAGS = -std=c89 
+CPPUTEST_CFLAGS += -Wall -Wstrict-prototypes -pedantic
+LD_LIBRARIES = -lpthread
+	
+  
+ifeq ($(CPPUTEST_HOME),)
+$(info CPPUTEST_HOME not set! See README.txt)
+else  
+include $(CPPUTEST_HOME)/build/MakefileWorker.mk
+endif
---
+++ code/tddc/tests/LedDriver/LedDriverTest.cpp
@@ -0,0 +1,79 @@
+//- ------------------------------------------------------------------
+//-    Copyright (c) James W. Grenning -- All Rights Reserved
+//-    For use by owners of Test-Driven Development for Embedded C,
+//-    and attendees of Renaissance Software Consulting, Co. training
+//-    classes.
+//-
+//-    Available at http://pragprog.com/titles/jgade/
+//-        ISBN 1-934356-62-X, ISBN13 978-1-934356-62-3
+//-
+//-    Authorized users may use this source code in your own
+//-    projects, however the source code may not be used to
+//-    create training material, courses, books, articles, and
+//-    the like. We make no guarantees that this source code is
+//-    fit for any purpose.
+//-
+//-    www.renaissancesoftware.net james@renaissancesoftware.net
+//- ------------------------------------------------------------------
+
+
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+#include "LedDriver.h"
+//#include "RuntimeErrorStub.h"
+}
+
+//START:final
+//START: RevisedTestGroupPlusCreate
+TEST_GROUP(LedDriver)
+{
+    uint16_t virtualLeds;
+
+    void setup()
+    {
+        LedDriver_Create(&virtualLeds);
+    }
+    void teardown()
+    {
+        LedDriver_Destroy();
+    }
+};
+//END: RevisedTestGroupPlusCreate
+
+TEST(LedDriver, LedsOffAfterCreate)
+{
+	//uint16_t virtualLeds = 0xffff;
+	LedDriver_Create(&virtualLeds);
+	CHECK_EQUAL(0,virtualLeds);
+}
+//END: StartingPoint
+
+TEST(LedDriver, TurnOnLedOne)
+{
+	LedDriver_TurnOn(1);
+	CHECK_EQUAL(1,virtualLeds);
+}
+
+TEST(LedDriver, TurnOffLedOne)
+{
+	LedDriver_TurnOn(1);
+	LedDriver_TurnOff(1);
+	CHECK_EQUAL(0,virtualLeds);
+}
+
+TEST(LedDriver, TurnOnMultipleLeds)
+{
+	LedDriver_TurnOn(9);
+	LedDriver_TurnOn(8);
+	CHECK_EQUAL(0x180,virtualLeds);
+}
+
+TEST(LedDriver, TurnOffAnyLed)
+{
+	LedDriver_TurnOn(9);
+	LedDriver_TurnOn(8);
+	LedDriver_TurnOff(8);
+	CHECK_EQUAL(0x180,virtualLeds);
+}
